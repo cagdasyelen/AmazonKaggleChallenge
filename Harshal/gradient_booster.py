21f4d299dd7bfa46ba82e259deffa84ee24de979
harpribot <harshalpriyadarshi6@gmail.com>
2016-03-27 13:30:21 -0500

    Refactors code: separates feature creation from Xgboost : separate handler for feature creation, loading and saving

--- Harshal/gradient_booster.py
+++ Harshal/gradient_booster.py
@@ -5,10 +5,12 @@ import numpy as np
 from scipy.sparse import csr_matrix,hstack
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import *
+from feature_creator import Feature_Handler
 
 class Xgboost:
     def __init__(self,K,dirName):
         self.K = K
+        self.handler = Feature_Handler()
         self.load_data_from_dir(dirName)
         self.xgb_model = xgb.XGBClassifier()
         self.classifier = self.cross_validator()
@@ -18,135 +20,30 @@ class Xgboost:
     def cross_validator(self):
         parameters = {
             'objective':['binary:logistic'],
-            'max_depth' :[6,8,10,12],
-            'n_estimators' : [300,500,800,1000,1400],
-            'colsample_bytree' : [0.5,0.7,1]
+            'max_depth' :[4,6,8,10],
+            'n_estimators' : [300,500,800,1000,1400,2000],
+            'colsample_bytree' : [0.7,0.8,1],
+            'learning_rate' :[0.1],
+            'scale_pos_weight' : [0.2]
         }
+
         clf = GridSearchCV(self.xgb_model,parameters, n_jobs=4,\
             cv=StratifiedKFold(self.train_Y, n_folds=5, shuffle=True), \
             verbose=2, refit=True)
 
         return clf
 
-
-
     def load_data_from_dir(self,dirName):
-        train_file = './'+ dirName + '/train.csv'
-        test_file = './' + dirName + '/test.csv'
-        df_train = pd.read_csv(train_file, header=0)
-        df_test = pd.read_csv(test_file, header=0)
+        df_train, df_test = self.handler.load_data_from_dir(dirName)
         # Add the relevant features
-        df_train = self.add_relevant_features(df_train, True)
-        df_test = self.add_relevant_features(df_test, False)
+        feature_set = df_train.columns.values[3:-1]
+        df_train, df_test = self.handler.feature_adder(df_train,df_test,feature_set)
         # Save the new train and test data
-        df_train.to_csv('data/train_with_feat.csv', index=False)
-        df_test.to_csv('data/test_with_feat.csv', index=False)
-        # Get train_X, train_y, test_X
-        train = df_train.values
-        self.train_X = train[0::,1::]
-        self.train_Y = train[0::,0]
-        self.test_X = df_test.values[0::,1::]
-        self.test_ID = df_test.values[0::,0]
+        self.handler.feature_saver(df_train,df_test)
+        # Get train_X, train_y, test_X, test_ID
+        self.train_X,self.train_Y,self.test_X,self.test_ID = \
+                        self.handler.get_feature_labels(df_train, df_test)
 
-
-    def add_relevant_features(self,df, isTrain):
-        # Add a unity column for counting purposes
-        unity_col = np.ones([df.shape[0],1]).astype(int)
-        df['UNITY'] = unity_col
-        # Get all the features in the original set
-        feature_set = df.columns.values[3:-1]
-        # Add the [frequency of resources handled by manager of the resources]
-        if isTrain:
-            self.table_res = df.pivot_table(index=['MGR_ID'], \
-                                    columns=['RESOURCE'], values=['UNITY'], \
-                                        aggfunc = np.sum, fill_value = 0).sum(1)
-            new_col = table[df['MGR_ID']]
-        else:
-            new_col = []
-            for mgr in df['MGR_ID']:
-                if mgr in self.table_res.index:
-                    new_col.append(self.table_res[mgr])
-                else:
-                    new_col.append(0)
-
-        df['MGR_RES'] = np.expand_dims(new_col,axis=1)
-
-        # Add the [number of accepted resources by the manager]
-        if isTrain:
-            self.table_accepted_res = df.pivot_table(index=['MGR_ID'], \
-                                    columns=['UNITY'], values=['ACTION'], \
-                                        aggfunc = np.sum, fill_value = 0).sum(1)
-            ''' @Note that (column,value) = (Unity,Action) === (Resource,Action)'''
-            new_col = self.table_rec[df['MGR_ID']]
-        else:
-            new_col = []
-            for mgr in df['MGR_ID']:
-                if mgr in self.table_accepted_res.index:
-                    new_col.append(self.table_accepted_res[mgr])
-                else:
-                    new_col.append(0)
-
-
-        df['MGR_ACCEPTED_RES'] = np.expand_dims(new_col,axis=1)
-
-        # Fraction of roles accepted for each of :
-        # 1. ROLE ROLLUP 1
-        # 2. ROLE ROLLUP 2
-        # 3. ROLE DEPT NAME
-        # 4. ROLE TITLE
-        # 5. ROLE_FAMILY_DESC
-        # 6. ROLE_FAMILY
-        # 7. ROLE_CODE
-
-        # dictionary of tables for all features to update the test data for
-        # acceptence related features
-        if isTrain:
-            self.table_feat = {}
-            self.table_feat_accepted = {}
-        # Loop over all the features
-        for feature in feature_set:
-            if isTrain:
-                # Total handled
-                self.table_feat[feature] = df.pivot_table(index=['MGR_ID',feature], \
-                                        columns=['RESOURCE'], values=['UNITY'], \
-                                            aggfunc = np.sum, fill_value = 0).sum(1)
-
-                # Total Accepted
-
-                self.table_feat_accepted[feature] = df.pivot_table(index=['MGR_ID',feature], \
-                                        columns=['UNITY'], values=['ACTION'], \
-                                            aggfunc = np.sum, fill_value = 0).sum(1)
-
-            # total -- Done using for loop : PLEASE IMPROVE
-            total_col = []
-            for mgr,feat in zip(df['MGR_ID'],df[feature]):
-                if isTrain or ((mgr,feat) in self.table_feat[feature].index):
-                    total_col.append(self.table_feat[feature][mgr,feat])
-                else:
-                    total_col.append(1)
-
-            total_col = np.array(total_col).astype(float)
-
-            # accepted -- Done using for loop : PLEASE IMPROVE
-            accepted_col = []
-            for mgr,feat in zip(df['MGR_ID'],df[feature]):
-                if isTrain or ((mgr,feat) in self.table_feat_accepted[feature].index):
-                    accepted_col.append(self.table_feat_accepted[feature][mgr,feat])
-                else:
-                    accepted_col.append(0)
-
-            accepted_col = np.array(accepted_col).astype(float)
-
-            # Get the fraction of acceptance
-            fraction_col = np.divide(accepted_col,total_col)
-
-            df['MGR_ACCEPT_FRAC_%s' %(feature)] = np.expand_dims(fraction_col,axis=1)
-
-
-        # Drop the UNITY col
-        df.drop('UNITY',axis=1,inplace=True)
-
-        return df
 
     def train(self):
         self.classifier.fit(self.train_X, self.train_Y)
